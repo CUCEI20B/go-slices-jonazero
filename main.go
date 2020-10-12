471eb01f87ecaba58ed89269db20010343f3f217
Jonathan Díaz <jonathan.dguevara@alumnos.udg.mx>
2020-10-12 15:12:59 -0500

    Slices

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
-
 import "fmt"
-
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main(){
+	var x uint = 0
+	var y uint = 0
+	var z uint = 0
+	fmt.Scan(&x)
+	s:=make([]uint,x)
+	for i:=0; i<int(x); i++{
+		fmt.Scan(&y)
+		s = append(s,y)
+		z += y
+	}
+	fmt.Print(z)
 }
